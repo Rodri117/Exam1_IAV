7e9b101087aafe02be1f2142bad2a2533a66b58d
Rodri117 <rodelgadoro@ucq.edu.mx>
2023-02-13 22:09:40 -0600

    Ultimos ajustes

--- Assets/Progra/Patrullage/Patrullar.cs
+++ Assets/Progra/Patrullage/Patrullar.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Patrullar : MonoBehaviour
 {
-    [SerializeField] private float v_velovidadMov;
+    [SerializeField] private float v_velovidadMov; // la velocidad en la que ira el guardia
 
-    [SerializeField] private Transform[] p_puntosMov;
+    [SerializeField] private Transform[] p_puntosMov;  //puntos que se movera
 
-    [SerializeField] private float d_distanciaMin;
+    [SerializeField] private float d_distanciaMin; // que tenga distancia
 
-    private int numAleatorio;
+    private int numAleatorio;  // controla a donde se va mover
     
 
-    private void Start()
+    private void Start() 
     {
-        numAleatorio = Random.Range(0, p_puntosMov.Length);
-        
+        numAleatorio = Random.Range(0, p_puntosMov.Length); // nuemro aleatorio
+
     }
 
     private void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, p_puntosMov[numAleatorio].position,
-            v_velovidadMov * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, p_puntosMov[numAleatorio].position,  // mover al guardia
+            v_velovidadMov * Time.deltaTime);                                                             // a donde queremos y la velocidad 
 
-        if(Vector2.Distance(transform.position, p_puntosMov[numAleatorio].position)<d_distanciaMin)
+        if(Vector2.Distance(transform.position, p_puntosMov[numAleatorio].position)<d_distanciaMin) //condicion
         {
-            numAleatorio = Random.Range(0, p_puntosMov.Length);
-            // Gira
+            numAleatorio = Random.Range(0, p_puntosMov.Length);  //cambiar a donde queremos que se mueva
+            // movimiento
         }
     }
 }
