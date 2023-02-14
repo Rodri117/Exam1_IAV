6eab58db80b10aa9dd2b02c26f3c42d04ee2ee67
Rodri117 <rodelgadoro@ucq.edu.mx>
2023-02-13 21:15:45 -0600

    EjObsta

---
+++ Assets/Progra/Obstaculos/MovControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovControl : MonoBehaviour
+{
+    private float v_velocidad = 4f;   //la velocidad del cubo
+    private Vector3 p_posicionObj;    //es la posicion en la que se movera con el raton
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        p_posicionObj = this.transform.position;   // inicializamos la posicion del cubo
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // el boton del raton hace click
+        {
+            p_posicionObj = Camera.main.ScreenToWorldPoint(Input.mousePosition);           //asigna la posicion de la pantalla con la del raton
+            p_posicionObj.z = this.transform.position.z;                                   //en un punto del mundo para posicionar el cubo
+        }
+        this.transform.position = Vector3.MoveTowards(this.transform.position, p_posicionObj, v_velocidad * Time.deltaTime);  //la velocidad a la que ira en el mundo
+    }
+}
